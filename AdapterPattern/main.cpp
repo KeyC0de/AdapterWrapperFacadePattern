898667549672c419771403d331c8fedd12f0b028
KeyC0de <nik.lazkey@gmail.com>
2022-02-20 13:27:13 +0200

    msvs2019

--- AdapterPattern/main.cpp
+++ AdapterPattern/main.cpp
@@ -3,17 +3,15 @@
 
 int main()
 {
-	fflush( stdout );
-	std::ios_base::sync_with_stdio( false );
-
-
-	std::unique_ptr<IRectangle> r = std::make_unique<Rectangle>( 120, 200, 60, 40 );
-	r->draw();
+	std::unique_ptr<IRectangle> pRect = std::make_unique<Rectangle>( 120, 200, 60, 40 );
+	pRect->draw();
 	std::cout << "\n\n";
 
-	Rectangle ra{10, 20, 40, 80};
-	ra.draw();
+	Rectangle rect{10, 20, 40, 80};
+	rect.draw();
 
-	std::system( "pause" );
-	return 0;
+#if defined _DEBUG && !defined NDEBUG
+	while ( !getchar() );
+#endif
+	return EXIT_SUCCESS;
 }
